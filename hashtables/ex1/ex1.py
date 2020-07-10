0b823a7e7aad64344a1cb3cfceba895948285af5
Pinefoot <jameslcarpino@gmail.com>
2020-07-10 08:33:59 -0700

    working through mvp, working on ex4.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,11 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
+    #store each weight in the input list as keys: 
+    if (weights, length, limit) in cache:
+        return cache[(weights, length, limit)]
+    elif weights <= 0:
+        cache[(weights, length, limit)] = 
     return None
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,14 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # first I need to set a dictionary to hold all the results
+    results = {}
+    # how does the cache hold the info
+    for pos_and_neg in a:
 
+        # what happens if the list has no numbers? return None
+        if a == 0:
+            return None
     return result
 
 
